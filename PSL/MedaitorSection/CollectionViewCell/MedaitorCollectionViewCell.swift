be9b7a03a6f31b75f24bebf4911dc6e02769bac2
Usama Shahid <usamashahid7666@gmail.com>
2021-03-23 21:55:10 +0500

    Medaitor view controller

---
+++ PSL/MedaitorSection/CollectionViewCell/MedaitorCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MedaitorCollectionViewCell.swift
+//  PSL
+//
+//  Created by MacBook on 23/03/2021.
+//
+
+import UIKit
+
+class MedaitorCollectionViewCell: UICollectionViewCell {
+    
+}
